155ad13529b39b1408220eeb5a6cf0b1059e17fb
Priyanka Singh <kumaripriyankasbg30@gmail.com>
2025-04-26 16:33:05 +0530

    Update db.js

--- server/lib/db.js
+++ server/lib/db.js
@@ -4,10 +4,10 @@ let connection;
 export const connectToDatabase = async () => {
     if(!connection){
        connection = await mysql.createConnection({
-           host: process.env.DB_HOST,
-           user: process.env.DB_USER,
-           password: process.env.DB_PASSWORD,
-           database: process.env.DB_NAME
+           host: process.env. MYSQLHOST,
+           user: process.env.MYSQLUSER,
+           password: process.env.MYSQLPASSWORD,
+           database: process.env.MYSQLDATABASE
        })
     }
     return connection;
