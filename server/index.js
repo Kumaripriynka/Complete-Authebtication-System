6b69f60ed63e9508a458734fc79a83b2785ee2e1
Priyanka Singh <kumaripriyankasbg30@gmail.com>
2025-04-26 17:00:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 // Routes
 app.use('/auth', authRouter);
 
-const PORT = process.env.MYSQLPORT || 3306;
+const PORT = process.env.PORT || 3300;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
