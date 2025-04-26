5a2703480b00d523747524dc38e802e0c108156e
Priyanka Singh <kumaripriyankasbg30@gmail.com>
2025-04-26 15:47:03 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/auth': {
-        target: 'http://localhost:3000',
+        target: 'https://complete-authentication-system-backend-2r3d.onrender.com',
         changeOrigin: true,
       },
     }
