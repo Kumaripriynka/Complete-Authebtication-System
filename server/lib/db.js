0284b6aed513ba65bc6009ae569d74bfd0d8499b
Priyanka Singh <kumaripriyankasbg30@gmail.com>
2025-04-26 16:54:12 +0530

    Update db.js

--- server/lib/db.js
+++ server/lib/db.js
@@ -8,6 +8,7 @@ export const connectToDatabase = async () => {
            user: process.env.MYSQLUSER,
            password: process.env.MYSQLPASSWORD,
            database: process.env.MYSQLDATABASE,
+           port: process.env.MYSQLPORT || 3306,
        })
     }
     return connection;
